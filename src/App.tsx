359497c9f1113ac1a29e4509ccf7f3abdef16257
nico <nico@luminocity.ch>
2026-04-08 16:07:38 +0200

    pages pubblish

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { FractalPage } from './pages/FractalPage';
 import './styles/globals.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<FractalPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
